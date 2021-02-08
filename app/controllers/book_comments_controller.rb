abbf66b35402040105aa39f4d22cd3b846281d5e
masaki-y1117 <my1117yamamoto@gmail.com>
2021-02-08 04:53:11 +0000

    [Finish]task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = @book.book_comments.build(book_comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    render :index
   end
   
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    render :index
   end
   
   private
